c812285987bba5c95911af932836c50374d1a6ec
carrliitos <salazar.ten80@gmail.com>
2020-08-05 12:09:15 -0500

    added ReasonerAPI

--- src/exploring/PythonFiles/ReasonerAPI.py
+++ src/exploring/PythonFiles/ReasonerAPI.py
@@ -1,10 +1,25 @@
 from biothings_explorer.user_query_dispatcher import SingleEdgeQueryDispatcher
 
 seqd = SingleEdgeQueryDispatcher(output_cls = "ChemicalSubstance", 
-								input_cls = "Gene",
+								input_cls = "Disease",
 								pred = "physically_interacts_with",
 								input_id = "NCBIGene",
 								values = "1017")
 
 query = seqd.query(verbose = True)
-print(query)
+
+output = seqd.to_reasoner_std() # Still need to read documentation for this method
+print()
+print(output.keys())
+# output: dict_keys(['query_graph', 'knowledge_graph', 'results'])
+queryGraph = output["query_graph"]
+knowledgeGraphNodes = output["knowledge_graph"]["nodes"][:5]
+knowledgeGraphEdges = output["knowledge_graph"]["edges"][:5]
+# output["results"][:5] 
+
+print("\nQuery Graph:")
+print(queryGraph)
+print("\nKnowledge Graph Nodes:")
+print(knowledgeGraphNodes)
+print("\nKnowledge Graph Edges:")
+print(knowledgeGraphEdges)
